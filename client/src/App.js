aaf96e3efe0e80b9bc542bc61868282fd0602188
vijay <vijaykumarkoneru13+github@gmail.com>
2023-04-18 01:00:38 +0530

    0.0.1.1

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
+import { Button } from "react-native";
 
 function App() {
+  const [name,setName] = useState("");
+  const [password,setPassword] = useState("");
+  const displayt = () =>{
+    console.log(name+"________"+password)
+
+  };
+  const cle =() =>{
+    console.clear()
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="container">
+      <div className="centered">
+    <label className="us">USER NAME</label><br/>
+    <input type="text" class="myInput" placeholder="USER NAME" onChange={(event) => {
+      setName(event.target.value)
+    }}/><br/>
+    <label className="pa">PASSWORD</label><br/>
+      <input  className="myInput" type="password" placeholder="PASSWORD" onChange={(event) => {
+      setPassword(event.target.value)
+    }}/><br/>
+    <div class="after"></div>
+    <button className="submit" id="submit" type="submit" onClick={displayt} >SUBMIT </button>
+    <button className="clear" id="clear" type="clear" onClick={cle}>CLEAR</button>
     </div>
+    </div>
+    </>
+
   );
 }
 
